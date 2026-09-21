7cd3796304e1dc9eb35b6766f0a4f5fd9adf5f73
HasanN2 <negahbanhasan77@gmail.com>
2026-09-21 12:30:05 +0330

    Fix Bug

--- apps/smartphones/views.py
+++ apps/smartphones/views.py
@@ -130,30 +130,3 @@ class PriceHistoryDetailAPIView(APIView):
         )
         
         
-class UserPreferenceAPIView(APIView):
-    permission_classes = [IsAuthenticated]
-    
-    def get(self,request) : 
-        preference = get_object_or_404(UserPreference,user=request.user)
-        serializer = UserPreferenceReadSerializer(preference)
-        return Response({"data" : serializer.data},status=status.HTTP_200_OK)
-    
-    def post(self, request):
-        user = request.user
-        serializer = UserPreferenceWriteSerializer(data=request.data)
-        if serializer.is_valid():
-            preference = serializer.save(user=user)
-            read_serializer = UserPreferenceReadSerializer(preference)
-            return Response({"data": read_serializer.data}, status=status.HTTP_200_OK)
-        return Response({"errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-        
-    
-    def patch(self,request) : 
-        user = request.user
-        preference = get_object_or_404(UserPreference,user=request.user)
-        serializer = UserPreferenceWriteSerializer(preference,data=request.data,partial=True)
-        if serializer.is_valid() : 
-            new_preferenc =serializer.save()
-            read_serializer = UserPreferenceReadSerializer(new_preferenc)
-            return Response({"data" : read_serializer.data},status=status.HTTP_200_OK)
-        return Response({"errors" : serializer.errors},status=status.HTTP_400_BAD_REQUEST)
